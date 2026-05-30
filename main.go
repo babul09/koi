7c796014aa55b4794e5d5c18552d54e15f2cf9fe
babul09 <babulbishwas09@gmail.com>
2026-05-30 22:29:22 +0530

    added compilation to webassembly

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-	// "github.com/go-gl/mathgl/mgl64"
+	// "github.com/go-gl/mathgl/mgl64
 	"image/color"
 	"log"
 	"strconv"
+	// "syscall/js"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
+const (
+	ScreenWidth  = 640
+	ScreenHeight = 360
+)
+
 type Game struct {
 	score int
+	xx    int
+	yy    int
 }
 
 func (g *Game) Update() error {
@@ -24,21 +32,23 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
-	var xx, yy = ebiten.CursorPosition()
+	g.xx, g.yy = ebiten.CursorPosition()
 
 	// ebitenutil.DebugPrint(screen, strconv.FormatFloat(ebiten.ActualFPS(), 'g', -1, 64))
 	ebitenutil.DebugPrint(screen, strconv.Itoa(g.score))
-	vector.StrokeCircle(screen, float32(xx), float32(yy), 20, 1, color.RGBA{245, 40, 145, 100}, true)
+	vector.StrokeCircle(screen, float32(g.xx), float32(g.yy), 20, 1, color.RGBA{245, 40, 145, 100}, true)
 
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 1920, 1300
+	return ScreenWidth, ScreenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(1920, 1300)
+	ebiten.SetWindowSize(ScreenWidth*2, ScreenHeight*2)
 	ebiten.SetWindowTitle("HEllo World")
+
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
