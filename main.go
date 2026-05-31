36ec90cbd9b821a52087bea261d5c27f4335c28a
babul09 <babulbishwas09@gmail.com>
2026-06-01 01:06:18 +0530

    implementing initial constraint model

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	// "github.com/go-gl/mathgl/mgl64
+	"fmt"
 	"image/color"
 	"log"
 	"strconv"
+
+	"github.com/go-gl/mathgl/mgl64"
+
 	// "syscall/js"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -15,29 +18,43 @@ import (
 const (
 	ScreenWidth  = 640
 	ScreenHeight = 360
+	Distance     = 50
 )
 
 type Game struct {
-	score int
-	xx    int
-	yy    int
+	score  int
+	xx     int
+	yy     int
+	Point  mgl64.Vec2
+	Anchor mgl64.Vec2
 }
 
 func (g *Game) Update() error {
 	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
 		g.score++
 	}
+	g.Anchor[0], g.Anchor[1] = float64(ScreenWidth/2), float64(ScreenHeight/2)
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
 	g.xx, g.yy = ebiten.CursorPosition()
+	g.Point[0], g.Point[1] = float64(g.xx), float64(g.yy)
 
+	delta := g.Point.Sub(g.Anchor)
+	constraint_Dist := delta.Normalize()
+
+	act_Point := constraint_Dist.Mul(Distance)
+	// fmt.Println(delta)
+	// fmt.Println(constraint_Dist.Len())
 	// ebitenutil.DebugPrint(screen, strconv.FormatFloat(ebiten.ActualFPS(), 'g', -1, 64))
+
 	ebitenutil.DebugPrint(screen, strconv.Itoa(g.score))
 	vector.StrokeCircle(screen, float32(g.xx), float32(g.yy), 20, 1, color.RGBA{245, 40, 145, 100}, true)
-
+	vector.FillCircle(screen, float32(g.Anchor[0]), float32(g.Anchor[1]), 5, color.RGBA{245, 100, 145, 100}, true)
+	vector.FillCircle(screen, float32(g.Anchor[0]+act_Point[0]), float32(g.Anchor[1]+act_Point[1]), 5, color.RGBA{245, 100, 145, 100}, true)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
