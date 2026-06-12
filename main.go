cf177ecba953c909b8915b0e4d6faef02e36f7fc
babul09 <babulbishwas09@gmail.com>
2026-06-12 15:50:49 +0530

    trying with vsync

--- main.go
+++ main.go
@@ -89,7 +89,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(ScreenWidth*2, ScreenHeight*2)
 	ebiten.SetWindowTitle("HEllo World")
-	ebiten.SetVsyncEnabled(false)
+	ebiten.SetVsyncEnabled(true)
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
